396ff16c9996987da45482fccd572c815c229c0e
Rafael Bellotti <rafael.bellotti@totvs.com.br>
2017-05-24 17:52:30 -0300

    education section in about tab

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+/*$( window ).resize(function() {
+  var maxHeight = 0;
+  $(".eduThumbnails").each(function(){ 
+        var thisHeight = $(this).height();
+        if (thisHeight > maxHeight){
+            maxHeight = thisHeight;
+        }
+  });
+  $(".eduThumbnails").each(function(){ 
+      $(this).height(maxHeight);
+  });
+});*/
