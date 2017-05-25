eaf25bc45e2715ac658edb945f76a13a03d8bba4
Rafael Bellotti <rafael.bellotti@totvs.com.br>
2017-05-25 18:51:39 -0300

    about me page color template done

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,19 @@
-/*$( window ).resize(function() {
-  var maxHeight = 0;
-  $(".eduThumbnails").each(function(){ 
-        var thisHeight = $(this).height();
-        if (thisHeight > maxHeight){
-            maxHeight = thisHeight;
+/*function resizeDivs(){
+    var maxHeight = 0;
+    $(".eduThumbnails").each(function(){ 
+        if ($(this).height() > maxHeight){
+            maxHeight = $(this).height();
         }
-  });
+    });
   $(".eduThumbnails").each(function(){ 
       $(this).height(maxHeight);
   });
-});*/
+    //$('body').append("max height:  " + maxHeight + '<br>');
+}
+
+$(window).resize(function() {
+    clearTimeout(window.resizedFinished);
+    window.resizedFinished = setTimeout(resizeDivs, 250);
+}); 
+
+//$(window).ready(resizeDivs);*/
